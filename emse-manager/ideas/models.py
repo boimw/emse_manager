7024113e31ab85ee3aae7223dced3c111137eddf
Bojan Marjanovic <marjanovic93@gmail.com>
2017-11-25 19:48:53 +0100

    feature (vote): add vote to idea

--- emse-manager/ideas/models.py
+++ emse-manager/ideas/models.py
@@ -2,13 +2,14 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import User
+from vote.models import VoteModel
 # Create your models here.
 
 class Category(models.Model):
 	name = models.CharField(max_length=50)
 	description = models.CharField(max_length=300)
 
-class Idea(models.Model):
+class Idea(VoteModel, models.Model):
 	name = models.CharField(max_length=50)
 	description = models.CharField(max_length=300)
 	catId = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -25,4 +26,4 @@ class Comment(models.Model):
 class Rating(models.Model):
 	userId = models.ForeignKey(User, on_delete=models.CASCADE)
 	ideaId = models.ForeignKey(Idea, on_delete=models.CASCADE)
-	rating = models.IntegerField(default=5)
+	rating = models.IntegerField(default=3)
